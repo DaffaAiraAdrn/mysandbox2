d7c6e618cbb529cb8a46c55141435fa679807410
DaffaAiraAdrn <whysoseriou0567@gmail.com>
2025-10-04 23:19:27 +0700

    Add files via upload

---
+++ 2411531006_TugasDesainWeb/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+  "/2411531006_TugasDesainWeb/index.html",
+  "/2411531006_TugasDesainWeb/about.html",
+  "/2411531006_TugasDesainWeb/contact.html",
+  "/2411531006_TugasDesainWeb/offline.html",
+  "/2411531006_TugasDesainWeb/global_style.css",
+  "/2411531006_TugasDesainWeb/icons/icon-192.png",
+  "/2411531006_TugasDesainWeb/icons/icon-512.png"
+  
+
+
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            Promise.all(
+                cacheNames.map((cache) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request)
+    )
+    );
+})
