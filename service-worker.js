356dd3a2554bdec96235642748a54b2ce739df05
DaffaAiraAdrn <whysoseriou0567@gmail.com>
2025-10-12 19:20:52 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -23,7 +23,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener("activate", (event) => {
     event.waitUntil(
         caches.keys().then((cacheNames) => {
-            Promise.all(
+            return Promise.all(
                 cacheNames.map((cache) => {
                     if (cache !== CACHE_NAME) {
                         return caches.delete(cache);
@@ -39,9 +39,10 @@ self.addEventListener("fetch", (event) => {
     fetch(event.request).catch(() => {
       
       return caches.match(event.request).then((response) => {
-        return response || caches.match("./offline.html");
+        return response || fetch(event.request).catch(() => caches.match("./offline.html"));
       });
     })
   );
 
 });
+
